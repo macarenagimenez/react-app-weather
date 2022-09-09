feb74a31d0bf715e503582dddba15f0b0a979388
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-09-09 11:23:45 -0300

    install bootstrap css

--- src/WeatherDescription.js
+++ src/WeatherDescription.js
@@ -5,6 +5,7 @@ import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 import "./WeatherDescription.css";
 
