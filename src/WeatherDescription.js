42304b7b049d5a4ed3e440e5202cb2f4867645e0
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2022-09-09 11:27:35 -0300

    delete button  bootstrap

--- src/WeatherDescription.js
+++ src/WeatherDescription.js
@@ -5,7 +5,6 @@ import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 
 import "./WeatherDescription.css";
 
